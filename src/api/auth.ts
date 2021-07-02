fb0634972eacba8d4f1c8a9875fa5793ea7a7a92
Kirill Shevchenko <stranger.net3@gmail.com>
2021-07-02 21:00:45 +0400

    Add auth API methods

---
+++ src/api/auth.ts
@@ -0,0 +1,26 @@
+import { IAuthLogin, IAuthMe } from '../types/auth';
+import { ServerData } from '../types/common';
+import instance from './instance';
+
+const auth = {
+  me: async () => {
+    const response = await instance.get<IAuthMe>('auth/me');
+
+    return response.data;
+  },
+
+  login: async (loginFormData: IAuthLogin) => {
+    const response = await instance.post<ServerData>(
+      'auth/login',
+      loginFormData
+    );
+
+    return response.data;
+  },
+
+  logout: async () => {
+    await instance.delete('auth/login');
+  },
+};
+
+export default auth;
---
+++ src/api/instance.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  withCredentials: true,
+  headers: {
+    'API-KEY': 'a3f656c8-219b-497c-b91e-7723447501cd',
+  },
+});
+
+export default instance;
