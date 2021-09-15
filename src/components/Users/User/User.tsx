240940231a9a74dc98540e8969de7d05add04b8a
Kirill Shevchenko <stranger.net3@gmail.com>
2021-09-15 21:38:18 +0400

    Refactor follow button in user

--- src/components/Users/User/User.tsx
+++ src/components/Users/User/User.tsx
@@ -2,24 +2,14 @@ import React from 'react';
 import { IUser } from '../../../types/users';
 import styles from './User.module.css';
 import photoPlaceholder from '../../../images/userPhoto.png';
-import { toggleFollow } from '../../../store/reducers/usersReducer';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { selectUsersState } from '../../../selectors/usersSelector';
-import classNames from 'classnames/bind';
-import Preloader from '../../Preloader/Preloader';
 import { NavLink } from 'react-router-dom';
+import FollowButton from '../../FollowButton/FollowButton';
 
 const User: React.FC<IUser> = ({ name, photos, followed, id }) => {
-  const cx = classNames.bind(styles);
-  const dispatch = useDispatch();
-
   const { inFollowProgress } = useSelector(selectUsersState);
   const photo = photos.large;
-  const isBtnDisabled = inFollowProgress.some(i => i === id);
-
-  const onToggleFollow = () => {
-    dispatch(toggleFollow(id));
-  };
 
   return (
     <li className={styles.user}>
@@ -34,23 +24,11 @@ const User: React.FC<IUser> = ({ name, photos, followed, id }) => {
             <span title={name}>{name}</span>
           </NavLink>
         </div>
-        <div
-          className={cx({
-            followBtn: true,
-            followed: followed,
-            disabled: isBtnDisabled,
-          })}
-        >
-          <button disabled={isBtnDisabled} onClick={onToggleFollow}>
-            {isBtnDisabled ? (
-              <Preloader size='15' color={followed ? '#000' : '#fff'} />
-            ) : followed ? (
-              'Unfollow'
-            ) : (
-              'Follow'
-            )}
-          </button>
-        </div>
+        <FollowButton
+          followed={followed}
+          userId={id}
+          inFollowProcess={inFollowProgress.some(i => i === id)}
+        />
       </div>
     </li>
   );
