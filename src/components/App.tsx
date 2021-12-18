9b83ad032e551531d6c13551f5ef0a0cc52f063d
Kirill Shevchenko <stranger.net3@gmail.com>
2021-12-18 18:20:14 +0400

    Fix lazy imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,40 +1,33 @@
 import React, {
+  lazy,
   Suspense,
   useEffect,
   useLayoutEffect,
-  useMemo,
   useState,
 } from 'react';
-import './App.css';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import { useDarkMode } from 'usehooks-ts';
 import { useDispatch } from '../hooks/useDispatch';
 import { useSelector } from '../hooks/useSelector';
-import { getUserAuthData } from '../store/reducers/authReducer';
-import { Menu } from './Menu/Menu';
-import { Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import { getAuthState } from '../selectors/authSelectors';
+import { getUserAuthData } from '../store/reducers/authReducer';
+import './App.css';
+import Chat from './Chat/Chat';
+import { Menu } from './Menu/Menu';
 import Preloader from './Preloader/Preloader';
 import { Profile } from './Profile/Profile';
-import { useDarkMode } from 'usehooks-ts';
-import Chat from './Chat/Chat';
+
+const NotFound = lazy(() => import('./NotFound/NotFound'));
+const Login = lazy(() => import('./Login/Login'));
+const Users = lazy(() => import('./Users/Users'));
+const Dialogs = lazy(() => import('./Dialogs/Dialogs'));
+const EditProfile = lazy(() => import('./Profile/EditProfile/EditProfile'));
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
   const { isAuth, id } = useSelector(getAuthState);
   const { isDarkMode } = useDarkMode(false);
   const [appInitialized, setAppInitialized] = useState(false);
-
-  const NotFound = useMemo(
-    () => React.lazy(() => import('./NotFound/NotFound')),
-    []
-  );
-  const Login = useMemo(() => React.lazy(() => import('./Login/Login')), []);
-  const Users = useMemo(() => React.lazy(() => import('./Users/Users')), []);
-  const Dialogs = useMemo(() => {
-    return React.lazy(() => import('./Dialogs/Dialogs'));
-  }, []);
-  const EditProfile = useMemo(() => {
-    return React.lazy(() => import('./Profile/EditProfile/EditProfile'));
-  }, []);
 
   useEffect(() => {
     dispatch(getUserAuthData()).then(() => setAppInitialized(true));
@@ -61,7 +54,6 @@ const App: React.FC = () => {
                     <Route path='login' element={<Outlet />} />
                     <Route path='profile' element={<Outlet />} />
                   </Route>
-
                   <Route path='/profile/:userId' element={<Profile />} />
                   <Route path='/profile/edit' element={<EditProfile />} />
                   <Route path='/users' element={<Users />} />
